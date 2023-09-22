40b0b4f85b8f0785f7cd022ce3c916c0b78914fd
koryanton <56415959+koryanton@users.noreply.github.com>
2023-09-22 16:08:56 -0400

    Adv.Prg.Hmwk 4 working

--- AdvProgramming_Homework_4.cpp
+++ AdvProgramming_Homework_4.cpp
@@ -1,6 +1,10 @@
-#include "opencv.hpp"
+//Eric Saldana 09/22/2023 "Advanced Robotic Programming - Assignment 4"
 
-
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+//I don't like using the scope resolution operator  so:
+using namespace std;
 using namespace cv;
 
 int main()
@@ -9,112 +13,114 @@ int main()
     const int MIN_MATCH_COUNT = 9;
 
     // Open the video capture (0 for webcam or provide a video file path)
-    VideoCapture cap("video.mp4"); // Replace "video.mp4" with your video file path
+    VideoCapture cap("video.mp4");
 
     // Check if the video capture is successful
     if (!cap.isOpened())
     {
-        std::cout << "Error: Video not found or cannot be opened." << std::endl;
+        cout << "Error: Video not found or cannot be opened." << endl;
         return -1;
     }
 
     // Create a SIFT object
-    Ptr<SIFT> sift = SIFT::create();
+    Ptr < SIFT > sift = SIFT::create();
 
-    // List of query images and their names
-    std::vector<std::pair<std::string, std::string>> query_images = {
-        {"chisel.png", "Chisel"}, // Replace with your query image filenames and names
-        {"RPi_Pico.png", "Pico"},
-        {"plug.png", "Plug"}};
+    // List of images to search for
+    vector < pair < string, string >> query_images = {
+                                                        {"chisel.png", "Chisel"},
+                                                        {"RPi_Pico.png", "Pico"},
+                                                        {"plug.png", "Plug"}
+                                                    };
 
     while (true)
     {
         // Capture frame-by-frame
         Mat frame;
-        bool ret = cap.read(frame);
+        cap.read(frame);
 
         // Check if the video has ended
-        if (!ret)
-            break;
-
-        for (const auto &query_pair : query_images)
+        if (frame.empty())
         {
-            const std::string &query_filename = query_pair.first;
-            const std::string &query_name = query_pair.second;
+            break;
+        }
+
+        for (const auto &query_info : query_images)
+        {
+            const string &query_filename = query_info.first;
+            const string &query_name = query_info.second;
 
             // Load the query image
             Mat img_query = imread(query_filename);
 
             // Find keypoints and descriptors in the frame and query image
-            std::vector<KeyPoint> kp1, kp2;
+            vector<KeyPoint> kp1, kp2;
             Mat des1, des2;
-            sift->detectAndCompute(img_query, noArray(), kp1, des1);
-            sift->detectAndCompute(frame, noArray(), kp2, des2);
+            sift->detectAndCompute(img_query, Mat(), kp1, des1);
+            sift->detectAndCompute(frame, Mat(), kp2, des2);
 
             // Algorithm Selection
-            const int FLANN_INDEX_KDTREE = 1;
+            int FLANN_INDEX_KDTREE = 1;
             // Get index parameters as a dictionary
-            std::map<int, int> index_params = {{cv::flann::KDTreeIndexParams::algorithm, FLANN_INDEX_KDTREE},
-                                               {cv::flann::KDTreeIndexParams::trees, 7}};
-            // Get search parameters
-            std::map<int, int> search_params = {{cv::flann::SearchParams::checks, 60}};
+            flann::IndexParams *index_params = new flann::KDTreeIndexParams();
             // Use Flann-based matcher
-            FlannBasedMatcher flann(index_params, search_params);
+            FlannBasedMatcher flann(index_params);
             // Get KNN matches for K value of 2
-            std::vector<std::vector<DMatch>> matches;
-            flann.knnMatch(des1, des2, matches, 2);
+            vector < vector<DMatch>> knn_matches;
+            flann.knnMatch(des1, des2, knn_matches, 2);
 
             // Store all the good matches
-            std::vector<DMatch> good;
-            for (size_t i = 0; i < matches.size(); i++)
+            vector<DMatch> good_matches;
+            for (size_t i = 0; i < knn_matches.size(); ++i)
             {
-                if (matches[i][0].distance < 0.75 * matches[i][1].distance)
-                    good.push_back(matches[i][0]);
+                if (knn_matches[i][0].distance < 0.75 * knn_matches[i][1].distance)
+                {
+                    good_matches.push_back(knn_matches[i][0]);
+                }
             }
 
-            if (good.size() > MIN_MATCH_COUNT)
+            if (good_matches.size() > MIN_MATCH_COUNT)
             {
-                // Get source and destination points and reshape arrays
-                std::vector<Point2f> src_pts, dst_pts;
-                for (const DMatch &match : good)
+                // Draw lines and set color
+                vector<Point2f> src_pts, dst_pts;
+                for (const DMatch &match : good_matches)
                 {
                     src_pts.push_back(kp1[match.queryIdx].pt);
                     dst_pts.push_back(kp2[match.trainIdx].pt);
                 }
-                Mat M = findHomography(src_pts, dst_pts, RANSAC, 5.0);
+                Mat H = findHomography(src_pts, dst_pts, RANSAC, 5.0);
+                vector<Point2f> obj_corners(4);
+                obj_corners[0] = Point2f(0, 0);
+                obj_corners[1] = Point2f(img_query.cols, 0);
+                obj_corners[2] = Point2f(img_query.cols, img_query.rows);
+                obj_corners[3] = Point2f(0, img_query.rows);
+                vector<Point2f> scene_corners(4);
+                perspectiveTransform(obj_corners, scene_corners, H);
 
-                // Matched mask list
-                std::vector<uchar> matchesMask(good.size(), 1);
+                // Convert scene_corners points to CV_32S format
+                vector<Point> scene_corners_int(scene_corners.size());
+                for (size_t i = 0; i < scene_corners.size(); ++i)
+                {
+                    scene_corners_int[i] = scene_corners[i];
+                }
 
-                // Get height, width, and depth of the frame
-                int h = img_query.rows;
-                int w = img_query.cols;
+                // Draw the polygon
+                polylines(frame, scene_corners_int, true, Scalar(0, 0, 255), 3, LINE_AA);
 
-                // Reshape points array to match height and width
-                std::vector<Point2f> pts = {{0, 0}, {0, h - 1}, {w - 1, h - 1}, {w - 1, 0}};
-                std::vector<Point2f> dst;
-                perspectiveTransform(pts, dst, M);
-
-                // Draw lines and set color
-                std::vector<Point> dst_int;
-                for (const Point2f &p : dst)
-                    dst_int.push_back(Point(static_cast<int>(p.x), static_cast<int>(p.y)));
-                polylines(frame, dst_int, true, Scalar(255, 200, 0), 3, LINE_AA);
-
-                // Display the object name
-                putText(frame, query_name, Point(50, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 2);
+                // Show frames
+                imshow("Matching Image", frame);
             }
+            
         }
 
-        // Show frames
-        imshow("Matching Image", frame);
-
         // Exit when 'q' is pressed
-        if (waitKey(1) & 0xFF == 'q')
+        if (waitKey(1) == 'q')
+        {
             break;
+        }
+
     }
 
-    // Release the video capture
+    // Release the video capture and close all OpenCV windows
     cap.release();
     destroyAllWindows();
 
