1edc745eaa695c3ade4c2ade7e7c2782eacc75cc
koryanton <56415959+koryanton@users.noreply.github.com>
2024-03-12 00:37:04 -0400

    MV assignemnt 1 finished

--- Python4Stu.py
+++ Python4Stu.py
@@ -1,17 +1,38 @@
-student_scores = {
-    "Harry": 81,
-    "Ron": 78,
-    "Hermione": 99,
-    "Draco": 74,
-    "Neville": 62,
-}
-# 🚨 Don't change the code above 👆
+import pandas as pd 
+import numpy as np
+import cv2
+import matplotlib.pylab as plt
 
-# TODO-1: Create an empty dictionary called student_grades.
-student_grades = {}
 
-# TODO-2: Write your code below to add the grades to student_grades.👇
-print(student_scores)
+# Load the original color image
+img_cv2 = cv2.imread('messi5.jpg')
 
-# 🚨 Don't change the code below 👇
-print(student_grades)
+# Convert the image to 8-bit (256 levels per channel)
+original_image_8bit = cv2.cvtColor(img_cv2, cv2.COLOR_BGR2RGB)
+
+# List to store modified images
+modified_images = []
+
+# Define the desired number of colors
+desired_color_levels = [6, 3]
+
+# Create a figure and subplots
+fig, axs = plt.subplots(1, len(desired_color_levels) + 1, figsize=(12, 4))
+
+# Display the original image
+axs[0].imshow(original_image_8bit)
+axs[0].set_title('Original Image')
+axs[0].axis('off')
+
+# Iterate over desired number of color levels
+for i, num_levels in enumerate(desired_color_levels):
+    # Quantize the image to the desired number of color levels
+    quantized_image = np.uint8(np.floor_divide(original_image_8bit, 256 // (2 ** num_levels)) * (256 // (2 ** num_levels)))
+
+    # Display the quantized image in subplot
+    axs[i+1].imshow(quantized_image)
+    axs[i+1].set_title(f'{2 ** num_levels} Colors')
+    axs[i+1].axis('off')
+
+plt.tight_layout()
+plt.show()
